9aa3d100926046e49fff2e18bf1e721f4c357c28
Tonmoy <tonmoysutradhar2002@gmail.com>
2025-08-29 20:32:32 +0600

    Initial setup done.

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-7xl font-bold text-center">Tonmoy Sutradhr</h1>
+    </div>
+  );
+}
+
+export default App;
