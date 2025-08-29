0ec91efc4ddc54368e513bdf59fdfed1ce9b0851
Tonmoy <tonmoysutradhar2002@gmail.com>
2025-08-29 21:00:53 +0600

    Basic structure design implemented.

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,12 @@ import React from "react";
 
 function App() {
   return (
-    <div>
-      <h1 className="text-7xl font-bold text-center">Tonmoy Sutradhr</h1>
+    <div className="w-[100%] min-h-screen flex items-center justify-center bg-purple-500 p-8">
+      <div className="bg-gray-100 shadow-2xl rounded-3xl p-8 w-full">
+        <h1 className="text-3xl font-bold text-center mb-6 text-indigo-600 underline">
+          SHIRITORI GAME
+        </h1>
+      </div>
     </div>
   );
 }
