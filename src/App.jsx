13d89652360f60fe8d324bd9bcafca43c3bf4a4d
Tonmoy <tonmoysutradhar2002@gmail.com>
2025-08-29 21:49:21 +0600

    Input field design and input the word in player.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function App() {
   const [turn, setTurn] = useState(1);
@@ -70,7 +70,7 @@ function App() {
 
   return (
     <div className="w-[100%] min-h-screen flex items-center justify-center bg-purple-500 p-8">
-      <div className="bg-gray-100 shadow-2xl rounded-3xl p-8 w-full">
+      <div className="bg-gray-100 shadow-2xl rounded-3xl p-8 w-[40%]">
         <h1 className="text-3xl font-bold text-center mb-6 text-indigo-600 underline">
           SHIRITORI GAME
         </h1>
@@ -78,39 +78,49 @@ function App() {
         {/* Player 1 */}
         <div className="flex justify-between items-center mb-6">
           <div
-          // className={`p-4 rounded-xl text-center w-40 ${
-          //   turn === 1
-          //     ? "bg-indigo-100 border-2 border-indigo-500"
-          //     : "bg-gray-100"
-          // }`}
+            className={`p-4 rounded-xl text-center w-40 ${
+              turn === 1
+                ? "bg-indigo-100 border-2 border-indigo-500"
+                : "bg-gray-100"
+            }`}
           >
             <h2 className="font-bold text-lg text-gray-800">Player 1</h2>
-            <p className="text-2xl font-extrabold text-blue-600">
-              {/* {scores.p1} */}
-              p1
-            </p>
+            <p className="text-2xl font-extrabold text-blue-600">{scores.p1}</p>
           </div>
 
           {/* Time */}
           <div className="text-center">
             <p className="text-gray-600 text-sm">Time Left</p>
-            <p className={"text-3xl font-bold "}>{timer}s 8s</p>
+            <p className={"text-3xl font-bold "}>{timer}s</p>
           </div>
 
           {/* player 2 */}
           <div
-          // className={`p-4 rounded-xl text-center w-40 ${
-          //   turn === 2
-          //     ? "bg-pink-100 border-2 border-pink-500"
-          //     : "bg-gray-100"
-          // }`}
+            className={`p-4 rounded-xl text-center w-40 ${
+              turn === 2
+                ? "bg-pink-100 border-2 border-pink-500"
+                : "bg-gray-100"
+            }`}
           >
             <h2 className="font-bold text-lg text-gray-800">Player 2</h2>
-            <p className="text-2xl font-extrabold text-pink-600">
-              {/* {scores.p2} */}
-              p2
-            </p>
+            <p className="text-2xl font-extrabold text-pink-600">{scores.p2}</p>
           </div>
+        </div>
+
+        {/* Input filed */}
+        <div className="flex gap-2 mb-6">
+          <input
+            value={word}
+            onChange={(e) => setWord(e.target.value)}
+            placeholder="Enter your word"
+            className="flex-grow px-4 py-3 rounded-2xl border-2 border-gray-200 focus:outline-none focus:ring-2 focus:ring-indigo-400"
+          />
+          <button
+            onClick={handleSubmit}
+            className="px-6 py-2 bg-indigo-600 text-white font-bold rounded-2xl"
+          >
+            Submit
+          </button>
         </div>
       </div>
     </div>
